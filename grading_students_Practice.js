ba0e8112da3bdb5de755f4a7faa5a4e69ac53c1a
duncanfgmoore <duncanfgmoore@gmail.com>
2018-11-19 19:12:26 -0600

    sutdent grades

---
+++ grading_students_Practice.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the gradingStudents function below.
+ */
+function gradingStudents(grades) {
+    /*
+     * Write your code here.
+     */
+
+    var index = 0;
+    while(index < grades.length) {
+        if (grades[index]%5 >=3 && grades[index] >37) {
+            grades[index] = grades[index] - grades[index]%5 + 5;
+            
+        }
+        
+        ++index;
+    }
+     return grades;
+    
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let grades = [];
+
+    for (let gradesItr = 0; gradesItr < n; gradesItr++) {
+        const gradesItem = parseInt(readLine(), 10);
+        grades.push(gradesItem);
+    }
+
+    let result = gradingStudents(grades);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
