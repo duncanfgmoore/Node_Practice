ce5e768eb27ae1fbecdcd1a843610f436b95851c
duncanfgmoore <duncanfgmoore@gmail.com>
2018-11-23 10:36:19 -0600

    sock merchant

---
+++ sock-merchant.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, socks) {
+      
+    socks.sort();
+    
+    var counter = 0;
+    for(var i = 0; i < socks.length; i++){
+        if(socks[i] === socks[i+1]){
+            counter++;
+            i++;  
+           //console.log(i)
+        }
+        //console.log(i)
+        //console.log(counter);
+    }
+    return counter;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
